7670d0b7d2bf548f7955d668a3ea5e4350f71ddf
hpblack99 <hankblack99@gmail.com>
2026-04-21 19:51:23 -0500

    Updated package.json

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,18 @@ import styles from './Navbar.module.css'
 export default function Navbar() {
   const location = useLocation()
 
-  const active = (path: string) =>
-    location.pathname === path ? styles.active : ''
-
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <Link to="/" className={styles.logo}>
-          GoyMogger
+          App
         </Link>
         <div className={styles.links}>
-          <Link to="/" className={`${styles.link} ${active('/')}`}>
+          <Link
+            to="/"
+            className={`${styles.link} ${location.pathname === '/' ? styles.active : ''}`}
+          >
             Home
-          </Link>
-          <Link to="/quoter" className={`${styles.link} ${active('/quoter')}`}>
-            FFE Quote Bot
           </Link>
         </div>
       </nav>
